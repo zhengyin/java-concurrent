36cde923eddba96bf6fd1d5abdf8a06954b58d68
zhengyin <zhengyin.name@gmail.com>
2020-09-15 08:46:29 +0800

    D+

--- src/main/java/com/izhengyin/demo/concurrent/part5/CasABA.java
+++ src/main/java/com/izhengyin/demo/concurrent/part5/CasABA.java
@@ -5,7 +5,9 @@ import com.izhengyin.demo.concurrent.SleepUtils;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicMarkableReference;
 import java.util.concurrent.atomic.AtomicStampedReference;
 
 /**
@@ -14,10 +16,14 @@ import java.util.concurrent.atomic.AtomicStampedReference;
  */
 public class CasABA {
     public static void main(String[] args) throws InterruptedException{
+
         System.out.println("abaProblem ");
         abaProblem();
         System.out.println("fixAbaProblem ");
         fixAbaProblem();
+        System.out.println("fixAbaProblem2 ");
+        fixAbaProblem2();
+
     }
 
     /**
@@ -47,6 +53,7 @@ public class CasABA {
      * @throws InterruptedException
      */
     private static void fixAbaProblem() throws InterruptedException{
+
         ExecutorService executorService = Executors.newFixedThreadPool(2);
         CountDownLatch countDownLatch = new CountDownLatch(2);
         final AtomicStampedReference<Integer> atomicInteger = new AtomicStampedReference<Integer>(1,0);
@@ -66,5 +73,34 @@ public class CasABA {
         countDownLatch.await();
         executorService.shutdown();
     }
+
+
+    /**
+     * 通过增加数据版本来避免ABA问题
+     * @throws InterruptedException
+     */
+    private static void fixAbaProblem2() throws InterruptedException{
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        final AtomicMarkableReference<Integer> atomicInteger = new AtomicMarkableReference<Integer>(1,false);
+        executorService.execute(() -> {
+            System.out.println(atomicInteger.compareAndSet(1,2,false,true) +" -> "+atomicInteger.getReference());
+            System.out.println(atomicInteger.compareAndSet(2,3,true,false) +" -> "+atomicInteger.getReference());
+            System.out.println(atomicInteger.compareAndSet(3,1,false,true) +" -> "+atomicInteger.getReference());
+            countDownLatch.countDown();
+        });
+        executorService.execute(() -> {
+            SleepUtils.sleep(100);
+            System.out.println(atomicInteger.compareAndSet(1,3,false,true) +" -> "+atomicInteger.getReference());
+            countDownLatch.countDown();
+        });
+        countDownLatch.await();
+        executorService.shutdown();
+    }
+
+
+
 
 }
---
+++ src/main/java/com/izhengyin/demo/concurrent/part6/LockTest.java
@@ -0,0 +1,10 @@
+package com.izhengyin.demo.concurrent.part6;
+
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @author zhengyin zhengyinit@outlook.com
+ * Created on 2020-09-14 17:15
+ */
+public class LockTest {
+}
